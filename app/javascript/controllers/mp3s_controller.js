cac9390eac1a2184d206f028bada8edb0d32c632
Greg Donald <gdonald@gmail.com>
2022-05-06 20:07:53 -0500

    stop form submit

--- app/javascript/controllers/mp3s_controller.js
+++ app/javascript/controllers/mp3s_controller.js
@@ -3,12 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
     const url = this.data.get('url')
-
-    $("#search_query").keyup( () => {
-      if ( event.which == 13 ) { event.preventDefault() }
-      this.search(url)
-    })
-
     this.search(url)
   }
 
