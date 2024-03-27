1ca18b7ed0530542b1598e1814ce026206d8cc58
Greg Donald <gd@darkclear.net>
2024-03-26 19:45:48 -0500

    bundle update

--- spec/models/queued_mp3_spec.rb
+++ spec/models/queued_mp3_spec.rb
@@ -1,5 +1,3 @@
-require 'rails_helper'
+# frozen_string_literal: true
 
-RSpec.describe QueuedMp3, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+require 'rails_helper'
